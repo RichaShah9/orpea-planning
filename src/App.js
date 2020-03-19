fac4a68c85b839a412cc78cb550b3d0e68081ad6
Richa <r.shah@axelor.com>
2020-03-19 17:47:51 +0530

    Modify view css

--- src/App.js
+++ src/App.js
@@ -2,33 +2,51 @@ import React from "react";
 import faker from "faker";
 import moment from "moment";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid, IconButton, Typography, Collapse } from "@material-ui/core";
+import {
+  Grid,
+  IconButton,
+  Typography,
+  Collapse,
+  Button,
+  Divider
+} from "@material-ui/core";
 import RefreshIcon from "@material-ui/icons/Refresh";
-import CollapseIcon from "@material-ui/icons/Remove";
-import ExpandIcon from "@material-ui/icons/Add";
+import { ArrowRight, ArrowDropDown } from "@material-ui/icons";
 
 import Popup from "./components/Popup";
 import DateHandler from "./components/DateHandler";
 import { COLORS } from "./constants";
-import AxelorService from './service/axelor.rest';
+import AxelorService from "./service/axelor.rest";
 
 import "./App.css";
 
 const MAX_SERVICE = 5;
 const MAX_PROFILE = 5;
 const MAX_EMPLOYEE = 10;
-const profileService = new AxelorService({model: "com.axelor.apps.orpea.planning.db.ProfileDay"});
-const employeeService = new AxelorService({model: "com.axelor.apps.orpea.planning.db.EmployeeDay"});
+const profileService = new AxelorService({
+  model: "com.axelor.apps.orpea.planning.db.ProfileDay"
+});
+const employeeService = new AxelorService({
+  model: "com.axelor.apps.orpea.planning.db.EmployeeDay"
+});
 
 const useStyles = makeStyles(theme => ({
   container: {
-    padding: 10
+    padding: 20,
+    height: "100%",
+    overflow: "hidden"
+  },
+  treeView: {
+    padding: "0px 20px 20px 20px",
+    width: "100%",
+    height: "calc(100% - 150px)",
+    overflow: "overlay"
   }
 }));
 
 function getColorFields() {
   const fields = [];
-  for(let i = 8; i <= 22; i++) {
+  for (let i = 8; i <= 22; i++) {
     fields.push(`h${i}ColorSelect`);
   }
   return fields;
@@ -80,18 +98,30 @@ function ColorGrid({ record, profile, employee, onColorChange }) {
   return (
     <Grid container justify="space-between">
       {getColorFields().map((key, i) => (
-          <Grid item key={i} align="center">
-            <Popup color={record[key]} profile={profile} employee={employee} onColorChange={(value) => onColorChange(key, value)} />
-          </Grid>
-        ))}
+        <Grid item key={i} align="center">
+          <Popup
+            color={record[key]}
+            profile={profile}
+            employee={employee}
+            onColorChange={value => onColorChange(key, value)}
+          />
+        </Grid>
+      ))}
     </Grid>
   );
 }
 
 function Employee({ profile, employee }) {
-  const onEmployeeChange = React.useCallback((key, value) => {
-    employeeService.save({id: employee.id, version: employee.version, [key]: value});
-  }, [employee]);
+  const onEmployeeChange = React.useCallback(
+    (key, value) => {
+      employeeService.save({
+        id: employee.id,
+        version: employee.version,
+        [key]: value
+      });
+    },
+    [employee]
+  );
 
   return (
     <>
@@ -99,7 +129,7 @@ function Employee({ profile, employee }) {
         <Grid container alignItems="center">
           <Grid item xs={2}></Grid>
           <Grid item xs={3}>
-            <Typography title={employee.name} noWrap>
+            <Typography title={employee.name} noWrap style={{ color: "gray" }}>
               {employee.name}
             </Typography>
           </Grid>
@@ -123,12 +153,20 @@ function Profile({ profile }) {
     setChecked(checked => !checked);
   }, []);
 
-  const Icon = checked ? CollapseIcon : ExpandIcon;
+  const Icon = checked ? ArrowDropDown : ArrowRight;
+  const color = checked ? "green" : "#2f4050";
 
   const { employees = [] } = profile;
-  const onProfileChange = React.useCallback((key, value) => {
-    profileService.save({id: profile.id, version: profile.version, [key]: value});
-  }, [profile]);
+  const onProfileChange = React.useCallback(
+    (key, value) => {
+      profileService.save({
+        id: profile.id,
+        version: profile.version,
+        [key]: value
+      });
+    },
+    [profile]
+  );
 
   return (
     <>
@@ -142,15 +180,21 @@ function Profile({ profile }) {
                   {profile.name}
                 </Typography>
               </Grid>
-              <Grid item>
-                <IconButton onClick={onClick} size="small">
-                  <Icon />
-                </IconButton>
-              </Grid>
+              {employees.length > 0 && (
+                <Grid item>
+                  <IconButton onClick={onClick} size="small">
+                    <Icon style={{ color }} />
+                  </IconButton>
+                </Grid>
+              )}
             </Grid>
           </Grid>
           <Grid item xs={7} align="center">
-            <ColorGrid record={profile} profile={profile} onColorChange={onProfileChange} />
+            <ColorGrid
+              record={profile}
+              profile={profile}
+              onColorChange={onProfileChange}
+            />
           </Grid>
           <Grid item xs={12}>
             <Collapse in={checked}>
@@ -174,21 +218,29 @@ function Service({ service }) {
     setChecked(checked => !checked);
   }, []);
 
-  const Icon = checked ? CollapseIcon : ExpandIcon;
+  const Icon = checked ? ArrowDropDown : ArrowRight;
+  const color = checked ? "green" : "#2f4050";
+
   return (
     <>
-      <Grid item xs={6}>
+      <Grid item xs={6} style={{ paddingTop: profiles.length === 0 ? 25 : 0 }}>
         <Grid container alignItems="center">
           <Grid item>
-            <Typography title={service.name} noWrap>
+            <Typography
+              title={service.name}
+              noWrap
+              style={{ fontWeight: "bold" }}
+            >
               {service.name}
             </Typography>
           </Grid>
-          <Grid item>
-            <IconButton onClick={onClick}>
-              <Icon />
-            </IconButton>
-          </Grid>
+          {profiles.length > 0 && (
+            <Grid item>
+              <IconButton onClick={onClick}>
+                <Icon fontSize="large" style={{ color }} />
+              </IconButton>
+            </Grid>
+          )}
         </Grid>
       </Grid>
       <Grid item xs={6}></Grid>
@@ -207,9 +259,14 @@ function Header({ onRefresh, date, onPrevious, onNext }) {
   return (
     <>
       <Grid item xs={1}>
-        <IconButton onClick={onRefresh}>
-          <RefreshIcon />
-        </IconButton>
+        <Button
+          variant="outlined"
+          startIcon={<RefreshIcon />}
+          onClick={onRefresh}
+          style={{ textTransform: "none" }}
+        >
+          Refresh
+        </Button>
       </Grid>
       <Grid item xs={2}></Grid>
       <Grid item xs={2}></Grid>
@@ -227,67 +284,91 @@ function View() {
     moment(new Date()).format("DD-MM-YYYY")
   );
 
-  const fetchData = React.useCallback(() => { 
-    const profileFields = ["service", "employee", "dayDate", "profile", ...getColorFields()];
+  const fetchData = React.useCallback(() => {
+    const profileFields = [
+      "service",
+      "employee",
+      "dayDate",
+      "profile",
+      ...getColorFields()
+    ];
     const employeeFields = [...profileFields, "profile"];
     const data = {
       criteria: [
-        {fieldName: 'dayDate', operator: '=', value: moment(date, "DD-MM-YYYY").format("YYYY-MM-DD")},
+        {
+          fieldName: "dayDate",
+          operator: "=",
+          value: moment(date, "DD-MM-YYYY").format("YYYY-MM-DD")
+        }
       ]
-    }
+    };
     const serviceList = [];
-    const getServiceIndex = (serviceId) => {
+    const getServiceIndex = serviceId => {
       return serviceList.findIndex(s => s.id === serviceId);
     };
     const getProfileIndex = (list, profileId) => {
       return list.findIndex(p => p.id === profileId);
-    }
-  
-    profileService.search({fields: profileFields, data}).then(res => {
-      employeeService.search({fields: employeeFields, data}).then(employeeResponse => {
-        const {data = []} = res;
-        const {data: employeeData = []} = employeeResponse;
-        const getProfile = (profile) => {
-          const _profile = data.find(p => p.profile.id === profile.id) || {};
-          const profileObject = {..._profile, name: profile.name, employees: []};
-          delete profileObject.profile;
-          delete profileObject.service;
-          return profileObject;
-        }
-  
-        employeeData.forEach(employee => {
-          const serviceIndex = getServiceIndex(employee.service.id);
-          const service = serviceIndex === -1 
-          ? {name: employee.service.name, id: employee.service.id, profiles: []} 
-          : serviceList[serviceIndex];
-          
-          const profileIndex = getProfileIndex(service.profiles, employee.id);
-          const profile = profileIndex === -1 ? getProfile(employee.profile) : service.profiles[profileIndex];
-          const empObject = {
-            name: employee.employee.name,
-            ...employee,
+    };
+
+    profileService.search({ fields: profileFields, data }).then(res => {
+      employeeService
+        .search({ fields: employeeFields, data })
+        .then(employeeResponse => {
+          const { data = [] } = res;
+          const { data: employeeData = [] } = employeeResponse;
+          const getProfile = profile => {
+            const _profile = data.find(p => p.profile.id === profile.id) || {};
+            const profileObject = {
+              ..._profile,
+              name: profile.name,
+              employees: []
+            };
+            delete profileObject.profile;
+            delete profileObject.service;
+            return profileObject;
           };
-          delete empObject.employee;
-          delete empObject.profile;
-          delete empObject.service;
-          profile.employees.push({
-            ...empObject,
+
+          employeeData.forEach(employee => {
+            const serviceIndex = getServiceIndex(employee.service.id);
+            const service =
+              serviceIndex === -1
+                ? {
+                    name: employee.service.name,
+                    id: employee.service.id,
+                    profiles: []
+                  }
+                : serviceList[serviceIndex];
+
+            const profileIndex = getProfileIndex(service.profiles, employee.id);
+            const profile =
+              profileIndex === -1
+                ? getProfile(employee.profile)
+                : service.profiles[profileIndex];
+            const empObject = {
+              name: employee.employee.name,
+              ...employee
+            };
+            delete empObject.employee;
+            delete empObject.profile;
+            delete empObject.service;
+            profile.employees.push({
+              ...empObject
+            });
+            if (profileIndex !== -1) {
+              service.profiles[profileIndex] = { ...profile };
+            } else {
+              service.profiles.push({ ...profile });
+            }
+            if (serviceIndex !== -1) {
+              serviceList[serviceIndex] = { ...service };
+            } else {
+              serviceList.push({ ...service });
+            }
           });
-          if(profileIndex !== -1) {
-            service.profiles[profileIndex] = {...profile};
-          } else {
-            service.profiles.push({...profile});
-          }
-          if(serviceIndex !== -1) {
-            serviceList[serviceIndex] = {...service}
-          } else {
-            serviceList.push({...service});
-          }
+          setData(serviceList);
         });
-        setData(serviceList);
-      });
     });
-  }, [date])
+  }, [date]);
 
   const onPrevious = React.useCallback(() => {
     setDate(
@@ -312,7 +393,7 @@ function View() {
 
   React.useEffect(() => {
     fetchData();
-  }, [fetchData])
+  }, [fetchData]);
 
   const classes = useStyles();
 
@@ -324,9 +405,15 @@ function View() {
         onPrevious={onPrevious}
         date={date}
       />
-      {data.map((service, i) => (
-        <Service service={service} key={i} />
-      ))}
+      <Divider style={{ width: "100%" }} />
+      <Grid className={classes.treeView}>
+        {data.map((service, i) => (
+          <React.Fragment key={i}>
+            <Service service={service} key={i} />
+            <Divider style={{ width: "100%", marginTop: 25 }} />
+          </React.Fragment>
+        ))}
+      </Grid>
     </Grid>
   );
 }
--- src/components/DateHandler.js
+++ src/components/DateHandler.js
@@ -12,31 +12,50 @@ const useStyles = makeStyles(theme => ({
     display: "inline-block",
     margin: 1,
     textAlign: "center"
+  },
+  typography: {
+    fontSize: 20
+  },
+  button: {
+    border: "1px solid lightgray",
+    borderRadius: 10,
+    padding: 8,
+    width: 60
+  },
+  container: { padding: "25px 0px" },
+  header: {
+    marginTop: 10,
+    paddingRight: 20
   }
 }));
 
 function DateHandler({ onPrevious = () => {}, onNext = () => {}, date }) {
   const classes = useStyles();
   return (
-    <Grid container alignItems="center" justify="space-between">
+    <Grid
+      container
+      alignItems="center"
+      justify="space-between"
+      className={classes.container}
+    >
       <Grid item xs={1}>
-        <IconButton onClick={onPrevious}>
+        <IconButton onClick={onPrevious} className={classes.button}>
           <PreviousIcon />
         </IconButton>
       </Grid>
       <Grid item xs={10} align="center">
-        <Typography>{date}</Typography>
+        <Typography className={classes.typography}>{date}</Typography>
       </Grid>
       <Grid item xs={1}>
-        <IconButton onClick={onNext}>
+        <IconButton onClick={onNext} className={classes.button}>
           <NextIcon />
         </IconButton>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={12} className={classes.header}>
         <Grid container justify="space-between">
           {new Array(15).fill(0).map((_, i) => (
             <Grid item key={i} className={classes.hour}>
-              <Typography>{i + 8}</Typography>
+              <Typography style={{ color: "gray" }}>{i + 8}</Typography>
             </Grid>
           ))}
         </Grid>
