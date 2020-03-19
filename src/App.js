139f0a35ca6c1e81855b37b25be08bf86a1b733d
Nishant Painter <n.painter@axelor.com>
2020-03-19 13:31:15 +0530

    Add fake display

--- src/App.js
+++ src/App.js
@@ -6,28 +6,117 @@ import { COLORS } from "./constants";
 
 import "./App.css";
 
+const MAX_SERVICE = 5;
+const MAX_PROFILE = 5;
+const MAX_EMPLOYEE = 10;
+
+function getRandomNumber(max = 5) {
+  return Math.floor(Math.random() * max);
+}
+
+function getColorHours() {
+  let hour = 8;
+  return new Array(15).fill(0).reduce(acc => {
+    acc[`color${hour++}h`] = COLORS[getRandomNumber()];
+    return acc;
+  }, {});
+}
+
+function getEmployee() {
+  return {
+    name: faker.name.firstName(),
+    ...getColorHours()
+  };
+}
+
+function getProfile() {
+  return {
+    name: faker.commerce.department(),
+    employees: new Array(MAX_EMPLOYEE).fill(0).map(getEmployee),
+    ...getColorHours()
+  };
+}
+
+function getService() {
+  return {
+    name: faker.company.companyName(),
+    profiles: new Array(MAX_PROFILE).fill(0).map(getProfile)
+  };
+}
+
 function getFakeData() {
-  return new Array(10).fill(0).map(() => ({
-    employee: { name: faker.name.firstName() },
-    profile: { name: faker.commerce.department() }
-  }));
+  return new Array(MAX_SERVICE).fill(0).map(getService);
+}
+
+function Employee({ profile, employee }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center"
+      }}
+    >
+      <h5>{employee.name}</h5>
+      <div>
+        {Object.keys(employee).map((key, i) =>
+          key.includes("color") ? (
+            <Popup
+              key={i}
+              color={employee[key]}
+              employee={employee}
+              profile={profile}
+            />
+          ) : null
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Profile({ profile }) {
+  const { employees = [] } = profile;
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center"
+        }}
+      >
+        <h4>Profile : {profile.name}</h4>
+        <div>
+          {Object.keys(profile).map((key, i) =>
+            key.includes("color") ? (
+              <Popup key={i} color={profile[key]} profile={profile} />
+            ) : null
+          )}
+        </div>
+      </div>
+      {employees.map((employee, i) => (
+        <Employee employee={employee} key={i} profile={profile} />
+      ))}
+    </>
+  );
 }
 
 function View() {
   const data = getFakeData();
-  return data.map(item => (
+
+  return (
     <div>
-      {new Array(15).fill(0).map((_, i) => {
+      {data.map((service, i) => {
+        const { profiles = [] } = service;
         return (
-          <Popup
-            key={i}
-            color={COLORS[Math.floor(Math.random() * 5)]}
-            {...item}
-          />
+          <React.Fragment key={i}>
+            <h3>Service : {service.name}</h3>
+            {profiles.map((profile, i) => {
+              return <Profile profile={profile} key={i} />;
+            })}
+          </React.Fragment>
         );
       })}
     </div>
-  ));
+  );
 }
 
 function App() {
