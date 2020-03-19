5cbd2cdde71ed50c47735fc5eeb6537b550e6185
Nishant Painter <n.painter@axelor.com>
2020-03-19 12:37:57 +0530

    Add fake demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from "react";
+import faker from "faker";
+
+import Popup from "./components/Popup";
+import { COLORS } from "./constants";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">orpea-planning</header>
+function getFakeData() {
+  return new Array(10).fill(0).map(() => ({
+    employee: { name: faker.name.firstName() },
+    profile: { name: faker.commerce.department() }
+  }));
+}
+
+function View() {
+  const data = getFakeData();
+  return data.map(item => (
+    <div>
+      {new Array(15).fill(0).map((_, i) => {
+        return (
+          <Popup
+            key={i}
+            color={COLORS[Math.floor(Math.random() * 5)]}
+            {...item}
+          />
+        );
+      })}
     </div>
-  );
+  ));
+}
+
+function App() {
+  return <View />;
 }
 
 export default App;
