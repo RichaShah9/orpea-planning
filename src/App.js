0fca5584ed195b6ecf82afd62e0e940aecc80e97
Amit Patel <a.patel@axelor.com>
2020-03-19 18:13:55 +0530

    Remove fake data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import faker from "faker";
 import moment from "moment";
 import { makeStyles } from "@material-ui/core/styles";
 import {
@@ -15,14 +14,10 @@ import { ArrowRight, ArrowDropDown } from "@material-ui/icons";
 
 import Popup from "./components/Popup";
 import DateHandler from "./components/DateHandler";
-import { COLORS } from "./constants";
 import AxelorService from "./service/axelor.rest";
 
 import "./App.css";
 
-const MAX_SERVICE = 5;
-const MAX_PROFILE = 5;
-const MAX_EMPLOYEE = 10;
 const profileService = new AxelorService({
   model: "com.axelor.apps.orpea.planning.db.ProfileDay"
 });
@@ -50,48 +45,6 @@ function getColorFields() {
     fields.push(`h${i}ColorSelect`);
   }
   return fields;
-}
-
-function getRandomNumber(max = 5, min = 0) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-function getColorHours() {
-  let hour = 8;
-  return new Array(15).fill(0).reduce(acc => {
-    acc[`color${hour++}h`] = COLORS[getRandomNumber(5, 0)];
-    return acc;
-  }, {});
-}
-
-function getEmployee() {
-  return {
-    name: faker.name.firstName(),
-    ...getColorHours()
-  };
-}
-
-function getProfile() {
-  return {
-    name: faker.commerce.department(),
-    employees: new Array(getRandomNumber(MAX_EMPLOYEE))
-      .fill(0)
-      .map(getEmployee),
-    ...getColorHours()
-  };
-}
-
-function getService() {
-  return {
-    name: faker.company.companyName(),
-    profiles: new Array(getRandomNumber(MAX_PROFILE)).fill(0).map(getProfile)
-  };
-}
-
-function getFakeData() {
-  return new Array(getRandomNumber(MAX_SERVICE)).fill(0).map(getService);
 }
 
 function ColorGrid({ record, profile, employee, onColorChange }) {
@@ -384,7 +337,6 @@ function View() {
         .add(1, "days")
         .format("DD-MM-YYYY")
     );
-    setData(getFakeData());
   }, [date]);
 
   const onRefresh = React.useCallback(() => {
