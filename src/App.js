b521a6770d0b190310595d80cf00d8b53f8ba4f8
r.shah <r.shah@axelor.com>
2020-03-25 16:23:10 +0530

    Add scrolls

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import MonthView from "./MonthView";
 import "./App.css";
 
+const useStyles = makeStyles(() => ({
+  main: {
+    overflow: "auto",
+    height: "100%"
+  }
+}));
+
 function App() {
-  return <MonthView />;
+  const classes = useStyles();
+  return (
+    <div className={classes.main}>
+      <MonthView />
+    </div>
+  );
 }
 
 export default App;
