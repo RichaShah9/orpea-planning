735c1c392617e9f430c2227df5c64b3fcb43bba8
Amit Patel <a.patel@axelor.com>
2020-03-26 16:26:57 +0530

    Update form for service list

--- src/LineForm.js
+++ src/LineForm.js
@@ -1,33 +1,49 @@
 import React from 'react';
-import {Dialog, DialogTitle, Select, MenuItem, DialogContent, DialogActions, Button } from '@material-ui/core';
+import {Dialog, DialogTitle, Select, MenuItem, DialogContent, DialogActions, Button, Typography } from '@material-ui/core';
 import AxelorService from "./service/axelor.rest";
 
 const employmentContractService = new AxelorService({
     model: "com.axelor.apps.hr.db.EmploymentContract"
 });
 
+const serviceAPI = new AxelorService({
+    model: "com.axelor.apps.orpea.planning.db.Service"
+});
+
 function LineForm({handleClose, open, fromDate, date}) {
+    const [services, setServices] = React.useState([]);
     const [contracts, setContracts] = React.useState([]);
-    const [contract, setContract] = React.useState('');
+    const [contract, setContract] = React.useState({employmentContractId: '', serviceId: ''});
 
     React.useEffect(() => {
         const data = {
-            _domain: 'self.companyDepartment = null',
+            // _domain: 'self.service = null',
         };
-        employmentContractService.search({fields: ['employee', 'companyDepartment'], data}).then(res => {
+        employmentContractService.search({fields: ['fullName', 'companyDepartment'], data}).then(res => {
             if(res && res.data) {
                 setContracts(res.data);
             }
         });
+        serviceAPI.search({fields: ['name']}).then(res => {
+            if(res && res.data) {
+              setServices(res.data);
+            }
+        })
     }, []);
 
     const handleChange = React.useCallback((e) => {
-        setContract(e.target.value);
+        console.log(e.target.name);
+        setContract(c => {
+            return {
+                ...c,
+                [e.target.name]: e.target.value,
+            }
+        });
     }, []);
 
     const handleSave = React.useCallback(() => {
         const context = {
-            employmentContractId: contract,
+            ...contract,
         }
         if(date) {
             context['lineDate'] = date;
@@ -36,11 +52,12 @@ function LineForm({handleClose, open, fromDate, date}) {
 
         }
         const object = {
-            action: 'com.axelor.apps.orpea.planning.web.EmploymentContractController:generateEmployeeDay',
+            action: 'com.axelor.apps.orpea.planning.web.EmploymentContractController:generateEmployeeMonth',
             data: {
                 ...context
             }
         }
+
         employmentContractService.action(object).then(res => {
             handleClose(true);
         });
@@ -49,14 +66,29 @@ function LineForm({handleClose, open, fromDate, date}) {
         <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open} fullWidth={true}>
             <DialogTitle id="simple-dialog-title">Add Employee</DialogTitle>
             <DialogContent>
+                <Typography style={{fontWeight: "bold"}}>Employement Contract</Typography>
                 <Select 
-                    value={contract}
+                    value={contract.employmentContractId}
                     onChange={handleChange}
-                    style={{ width: '100%'}}
+                    style={{ width: '100%', marginBottom: 10}}
+                    name="employmentContractId"
                 >
                     {
                         contracts.map((c, i) => (
-                            <MenuItem key={i} value={c.id}>{c.employee && c.employee.name}</MenuItem>
+                            <MenuItem key={i} value={c.id}>{c.fullName}</MenuItem>
+                        ))
+                    }
+                </Select>
+                <Typography style={{fontWeight: "bold"}}>Service</Typography>
+                <Select 
+                    value={contract.serviceId}
+                    onChange={handleChange}
+                    style={{ width: '100%'}}
+                    name="serviceId"
+                >
+                    {
+                        services.map((c, i) => (
+                            <MenuItem key={i} value={c.id}>{c.name}</MenuItem>
                         ))
                     }
                 </Select>
