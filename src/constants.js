b0467699f3146cde2b8ba02146b11255fafd2817
Nishant Painter <n.painter@axelor.com>
2020-03-26 11:21:47 +0530

    Update colors

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,8 @@
-export const OLD_COLORS = [
-  "#4caf50", // Green // Colors are material color 500
-  "#2196f3", // Blue
-  "#ffeb3b", // Yellow
-  "#ff9800", // Orange
-  "#f44336", // Red
-  "#FFFFFF" // White
-];
-
 export const COLORS = [
-  "#81c784", // Green // Colors are material color 300
-  "#64b5f6", // Blue
-  "#fff176", // Yellow
-  "#ffb74d", // Orange
-  "#e57373", // Red
+  "#7fbc64", // Green
+  "#7ba3ed", // Blue
+  "#ffec6e", // Yellow
+  "#ffc55e", // Orange
+  "#ef5350", // Red
   "#FFFFFF" // White
 ];
