09ec6730b60bb5a2468ce0b813fb34d79e45d0f5
Nishant Painter <n.painter@axelor.com>
2020-03-30 17:15:29 +0530

    Add profile filter in add employee

--- src/LineForm.js
+++ src/LineForm.js
@@ -9,7 +9,7 @@ import {
   Button,
   Typography
 } from "@material-ui/core";
-import moment from 'moment';
+import moment from "moment";
 import AxelorService from "./service/axelor.rest";
 
 const employmentContractService = new AxelorService({
@@ -19,7 +19,13 @@ const serviceAPI = new AxelorService({
   model: "com.axelor.apps.orpea.planning.db.Service"
 });
 
+const profileService = new AxelorService({
+  model: "com.axelor.apps.orpea.planning.db.Profile"
+});
+
 function LineForm({ handleClose, open, fromDate, date }) {
+  const [profile, setProfile] = React.useState("");
+  const [profiles, setProfiles] = React.useState([]);
   const [services, setServices] = React.useState([]);
   const [contracts, setContracts] = React.useState([]);
   const [contract, setContract] = React.useState({
@@ -28,22 +34,34 @@ function LineForm({ handleClose, open, fromDate, date }) {
   });
 
   React.useEffect(() => {
-    const data = {
-      _domain: 'self.service = null',
-    };
-    employmentContractService
-      .search({ fields: ["fullName", "companyDepartment"], data })
-      .then(res => {
-        if (res && res.data) {
-          setContracts(res.data);
-        }
-      });
+    profileService.search({ fields: ["name"] }).then(res => {
+      if (!(res && res.data && res.data.length)) return;
+      setProfiles(res.data);
+      setProfile(res.data[0].id);
+    });
+
     serviceAPI.search({ fields: ["name"] }).then(res => {
       if (res && res.data) {
         setServices(res.data);
       }
     });
   }, []);
+
+  React.useEffect(() => {
+    if (!profile) return;
+    const data = {
+      _domain: `self.service = null and self.profile = ${profile}`
+    };
+    employmentContractService
+      .search({ fields: ["fullName", "companyDepartment"], data })
+      .then(res => {
+        if (res && res.data) {
+          setContracts(res.data);
+        } else {
+          setContracts([]);
+        }
+      });
+  }, [profile]);
 
   const handleChange = React.useCallback(e => {
     setContract(c => {
@@ -85,6 +103,18 @@ function LineForm({ handleClose, open, fromDate, date }) {
     >
       <DialogTitle id="simple-dialog-title">Ajouter employé </DialogTitle>
       <DialogContent>
+        <Typography style={{ fontWeight: "bold" }}>Qualification</Typography>
+        <Select
+          value={profile}
+          onChange={({ target: { value } }) => setProfile(value)}
+          style={{ width: "100%" }}
+        >
+          {profiles.map((p, i) => (
+            <MenuItem key={i} value={p.id}>
+              {p.name}
+            </MenuItem>
+          ))}
+        </Select>
         <Typography style={{ fontWeight: "bold" }}>Contrat employé</Typography>
         <Select
           value={contract.employmentContractId}
